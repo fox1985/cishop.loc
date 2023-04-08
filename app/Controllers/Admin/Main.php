bf88f091da7a8e1b912eb849ddfde689ba0dc7b2
fox1985 <artemdav2@gmail.com>
2023-04-08 20:27:34 +0600

    new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	new file:   app/.htaccess 	new file:   app/Common.php 	new file:   app/Config/App.php 	new file:   app/Config/Autoload.php 	new file:   app/Config/Boot/development.php 	new file:   app/Config/Boot/production.php 	new file:   app/Config/Boot/testing.php 	new file:   app/Config/CURLRequest.php 	new file:   app/Config/Cache.php 	new file:   app/Config/Constants.php 	new file:   app/Config/ContentSecurityPolicy.php 	new file:   app/Config/Cookie.php 	new file:   app/Config/Database.php 	new file:   app/Config/DocTypes.php 	new file:   app/Config/Email.php 	new file:   app/Config/Encryption.php 	new file:   app/Config/Events.php 	new file:   app/Config/Exceptions.php 	new file:   app/Config/Feature.php 	new file:   app/Config/Filters.php 	new file:   app/Config/ForeignCharacters.php 	new file:   app/Config/Format.php 	new file:   app/Config/Generators.php 	new file:   app/Config/Honeypot.php 	new file:   app/Config/Images.php 	new file:   app/Config/Kint.php 	new file:   app/Config/Logger.php 	new file:   app/Config/Migrations.php 	new file:   app/Config/Mimes.php 	new file:   app/Config/Modules.php 	new file:   app/Config/Pager.php 	new file:   app/Config/Paths.php 	new file:   app/Config/Publisher.php 	new file:   app/Config/Routes.php 	new file:   app/Config/Security.php 	new file:   app/Config/Services.php 	new file:   app/Config/Session.php 	new file:   app/Config/Toolbar.php 	new file:   app/Config/UserAgents.php 	new file:   app/Config/Validation.php 	new file:   app/Config/View.php 	new file:   app/Controllers/Admin/Main.php 	new file:   app/Controllers/Admin/User.php 	new file:   app/Controllers/BaseController.php 	new file:   app/Controllers/Home.php 	new file:   app/Controllers/Main.php 	new file:   app/Database/Migrations/.gitkeep 	new file:   app/Database/Seeds/.gitkeep 	new file:   app/Filters/.gitkeep 	new file:   app/Helpers/.gitkeep 	new file:   app/Language/.gitkeep 	new file:   app/Language/en/Validation.php 	new file:   app/Libraries/.gitkeep 	new file:   app/Models/.gitkeep 	new file:   app/ThirdParty/.gitkeep 	new file:   app/Views/errors/cli/error_404.php 	new file:   app/Views/errors/cli/error_exception.php 	new file:   app/Views/errors/cli/production.php 	new file:   app/Views/errors/html/debug.css 	new file:   app/Views/errors/html/debug.js 	new file:   app/Views/errors/html/error_404.php 	new file:   app/Views/errors/html/error_exception.php 	new file:   app/Views/errors/html/production.php 	new file:   app/Views/welcome_message.php 	new file:   app/index.html 	new file:   builds 	new file:   composer.json 	new file:   composer.lock 	new file:   phpunit.xml.dist 	new file:   preload.php 	new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   spark 	new file:   tests/README.md 	new file:   tests/_support/Database/Migrations/2020-02-22-222222_example_migration.php 	new file:   tests/_support/Database/Seeds/ExampleSeeder.php 	new file:   tests/_support/Libraries/ConfigReader.php 	new file:   tests/_support/Models/ExampleModel.php 	new file:   tests/database/ExampleDatabaseTest.php 	new file:   tests/session/ExampleSessionTest.php 	new file:   tests/unit/HealthTest.php 	new file:   writable/.htaccess 	new file:   writable/cache/index.html 	new file:   writable/logs/index.html 	new file:   writable/session/index.html 	new file:   writable/uploads/index.html

---
+++ app/Controllers/Admin/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Main extends BaseController
+{
+    public function index()
+    {
+        //echo __METHOD__;
+    }
+}
